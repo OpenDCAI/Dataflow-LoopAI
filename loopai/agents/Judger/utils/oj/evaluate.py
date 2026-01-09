817e12a8419aa1026fdadb6df1596a384a641067
baobaobao <82161965+brjbrj@users.noreply.github.com>
2026-01-09 15:28:47 +0800

    Refactor evaluation functions and add logging
    Refactor evaluation logic to calculate pass@k and write logs.

--- loopai/agents/Judger/utils/oj/evaluate.py
+++ loopai/agents/Judger/utils/oj/evaluate.py
@@ -7,10 +7,10 @@ from datetime import datetime, date, time
 import numpy as np
 import tqdm
 from .data import write_jsonl, stream_jsonl, read_problems
+from .execution_sql import compare_sql_wrapper
 from .execution import check_correctness
 from loopai.logger import get_logger
 logger = get_logger()
-
 def estimate_pass_at_k(
     num_samples: Union[int, List[int], np.ndarray],
     num_correct: Union[List[int], np.ndarray],
@@ -36,18 +36,53 @@ def estimate_pass_at_k(
 
     return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)])
 
-def evaluate_functional_correctness(K, n_workers, timeout, test_case_path, problem_path, result_path):
+def culculate_pass_at_k(k, results):
+    total, correct = [], []
+    for result in results.values():
+        result.sort()
+        passed = [r[1]["passed"] for r in result]
+        total.append(len(passed))
+        correct.append(sum(passed))
+    total = np.array(total)
+    correct = np.array(correct)
+
+    ks = k
+    pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
+                 for k in ks if (total >= k).all()}
+    return pass_at_k
+
+def write_evaluate_log(pass_at_k, result_path, test_case_path, problem_path):
+    file_path = os.path.join(os.path.dirname(result_path),"log.txt")
+    pass_at_k_text = "\n".join([
+        f"{key}: {value:.4f}"
+        for key, value in pass_at_k.items()
+    ])
+    with open(file_path, mode='a', encoding='utf-8') as f:
+        f.write("\n\n--- " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ---")
+        f.write("\nSample:" + test_case_path)
+        f.write("\nProblem:" + problem_path)
+        f.write("\nResult:" + result_path)
+        f.write("\n" + pass_at_k_text)
+
+def evaluate_sample_sql(state):
     """
     Evaluates the functional correctness of generated samples, and writes
-    results to f"{sample_file}_results.jsonl.gz"
+    results to f"{test_case_path}_results.jsonl.gz"
     """
-    sample_file = test_case_path
-    problem_file = problem_path
+    K = '1,10,100'
+    n_workers=1
+    timeout=3.0
+
+    judger_state = state.get("judger", {})
+    test_case_path = judger_state.get("eval_test_case_path", "")
+    problem_path = judger_state.get('eval_problem_path', "")
+    result_path = judger_state.get('eval_result_path', "")
+
     k = list(map(int, K.split(",")))
     n_workers = n_workers
     timeout = timeout
 
-    problems = read_problems(problem_file)
+    problems = read_problems(problem_path)
 
     """
     Check the generated samples against test suites.
@@ -60,7 +95,77 @@ def evaluate_functional_correctness(K, n_workers, timeout, test_case_path, probl
         results = defaultdict(list)
 
         logger.info("Reading samples...")
-        for sample in tqdm.tqdm(stream_jsonl(sample_file)):
+        for sample in tqdm.tqdm(stream_jsonl(test_case_path)):
+            task_id = sample["task_id"]
+            db_file = sample["db_file"]
+            question = sample["question"]
+            ground_truth = sample["ground_truth"]
+            pred_sql = sample["completion"]
+            args = ((task_id, db_file, question, ground_truth, pred_sql), timeout, completion_id[task_id])
+            future = executor.submit(compare_sql_wrapper, *args)
+            futures.append(future)
+            completion_id[task_id] += 1
+            n_samples += 1
+
+        assert len(completion_id) == len(problems), "Some problems are not attempted."
+
+        logger.info("Running test suites...")
+        for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
+            result = future.result()
+            results[result["task_id"]].append((result["completion_id"], result))
+
+    """Calculate pass@k."""
+    pass_at_k = culculate_pass_at_k(k, results)
+
+    """Save the results in one file"""
+    def combine_results():
+        for sample in stream_jsonl(test_case_path):
+            task_id = sample["task_id"]
+            result = results[task_id].pop(0)
+            sample["result"] = result[1]["result"]
+            sample["passed"] = result[1]["passed"]
+            yield sample
+
+    logger.info(f"Writing results to {result_path}...")
+    write_jsonl(result_path, tqdm.tqdm(combine_results(), total=n_samples))
+
+    # 写入评测日志
+    write_evaluate_log(pass_at_k, result_path, test_case_path, problem_path)
+
+    return pass_at_k
+
+def evaluate_sample(state):
+    """
+    Evaluates the functional correctness of generated samples, and writes
+    results to f"{test_case_path}_results.jsonl.gz"
+    """
+    K = '1,10,100'
+    n_workers=1
+    timeout=3.0
+
+    judger_state = state.get("judger", {})
+    test_case_path = judger_state.get("eval_test_case_path", "")
+    problem_path = judger_state.get('eval_problem_path', "")
+    result_path = judger_state.get('eval_result_path', "")
+
+    k = list(map(int, K.split(",")))
+    n_workers = n_workers
+    timeout = timeout
+
+    problems = read_problems(problem_path)
+
+    """
+    Check the generated samples against test suites.
+    """
+    with ThreadPoolExecutor(max_workers=n_workers) as executor:
+
+        futures = []
+        completion_id = Counter()
+        n_samples = 0
+        results = defaultdict(list)
+
+        logger.info("Reading samples...")
+        for sample in tqdm.tqdm(stream_jsonl(test_case_path)):
             task_id = sample["task_id"]
             completion = sample["completion"]
             args = (problems[task_id], completion, timeout, completion_id[task_id])
@@ -77,51 +182,21 @@ def evaluate_functional_correctness(K, n_workers, timeout, test_case_path, probl
             results[result["task_id"]].append((result["completion_id"], result))
 
     """Calculate pass@k."""
-    total, correct = [], []
-    for result in results.values():
-        result.sort()
-        passed = [r[1]["passed"] for r in result]
-        total.append(len(passed))
-        correct.append(sum(passed))
-    total = np.array(total)
-    correct = np.array(correct)
+    pass_at_k = culculate_pass_at_k(k, results)
 
-    ks = k
-    pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
-                 for k in ks if (total >= k).all()}
-
-    """Finally, save the results in one file"""
+    """Save the results in one file"""
     def combine_results():
-        for sample in stream_jsonl(sample_file):
+        for sample in stream_jsonl(test_case_path):
             task_id = sample["task_id"]
             result = results[task_id].pop(0)
             sample["result"] = result[1]["result"]
             sample["passed"] = result[1]["passed"]
             yield sample
 
-    out_file = result_path
-    logger.info(f"Writing results to {out_file}...")
-    write_jsonl(out_file, tqdm.tqdm(combine_results(), total=n_samples))
+    logger.info(f"Writing results to {result_path}...")
+    write_jsonl(result_path, tqdm.tqdm(combine_results(), total=n_samples))
 
-    file_path = os.path.join(os.path.dirname(out_file),"log.txt")
-    pass_at_k_text = "\n".join([
-        f"{key}: {value:.4f}"
-        for key, value in pass_at_k.items()
-    ])
-    with open(file_path, mode='a', encoding='utf-8') as f:
-        f.write("\n\n--- " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " ---")
-        f.write("\nSample:" + test_case_path)
-        f.write("\nProblem:" + problem_path)
-        f.write("\nResult:" + result_path)
-        f.write("\n" + pass_at_k_text)
+    # 写入评测日志
+    write_evaluate_log(pass_at_k, result_path, test_case_path, problem_path)
 
     return pass_at_k
-
-def evaluate_sample(K, n_workers, timeout, test_case_path, problem_path, result_path):
-    """
-    Evaluates the functional correctness of generated samples, and writes
-    results to f"{sample_file}_results.jsonl.gz"
-    """
-    results = evaluate_functional_correctness(K, n_workers, timeout, test_case_path, problem_path, result_path)
-    logger.info(results)
-    return results
