51cf2cb3cab1ebc192d6327099042c0c591dd6ee
baobaobao <82161965+brjbrj@users.noreply.github.com>
2026-01-16 16:38:36 +0800

    Refactor sample generation and update max tokens
    Refactor sample generation logic to improve clarity and efficiency. Update max tokens and adjust handling of sample counts.

--- loopai/agents/Judger/utils/oj/generate.py
+++ loopai/agents/Judger/utils/oj/generate.py
@@ -10,19 +10,8 @@ from loopai.schema.states import LoopAIState
 from loopai.agents import BaseAgent
 
 from loopai.logger import get_logger
+
 logger = get_logger()
-
-def filter_code(completion: str) -> str:
-    """
-    过滤代码code格式的函数
-    Args:
-        - completion: 生成的或者是传入的代码文本
-    Returns:
-        去除最左边的换行，并选取第一次出现连续两次换行符前的内容
-    """
-    completion = completion.lstrip("\n")
-    return completion.split("\n\n")[0]
-
 
 def init_model(model_path: str, base_url: str, api_key: str, temperature: float = 0, top_p: float = 0.95):
     model = ChatOpenAI(
@@ -31,7 +20,7 @@ def init_model(model_path: str, base_url: str, api_key: str, temperature: float 
         base_url=base_url,
         temperature=temperature,
         top_p=top_p,
-        max_tokens=8192
+        max_tokens=16384
     )
     return model
 
@@ -58,8 +47,8 @@ def generate_sample(state):
     test_case_path = judger_state['eval_test_case_path']
     problem_path = judger_state['eval_problem_path']
     batch_size = judger_state['eval_batch_size']
-    num_samples_per_task = batch_size
-
+    num_samples_per_task = judger_state['eval_case_num']
+    
     problems = read_problems(problem_path)
     all_task_ids = list(problems.keys())
     total_tasks = len(all_task_ids)
@@ -69,7 +58,7 @@ def generate_sample(state):
         writer(StreamEvent(
             current=state['current'],
             progress=0.0,
-            message="常规任务样本合成开始",
+            message="code任务样本合成开始",
             data={"msg": f"任务总数：{total_tasks}|每个任务样本数：{num_samples_per_task}|总样本数：{total_samples}"}
         ).json())
     logger.info(f"\n===== 开始生成样本 =====")
@@ -80,35 +69,31 @@ def generate_sample(state):
     samples = []
     response_ptr = 0
     cnt = 0
-    for batch_idx in range(0, total_tasks, batch_size):
-        batch_task_ids = all_task_ids[batch_idx:batch_idx + batch_size]
-        
-        prompts = []
-        batch_task_id_list = []
-        t = 0
-        for task_id in batch_task_ids:
-            prompt = problems[task_id]['prompt']
-            for i in range(0,num_samples_per_task,1):
-                prompts.append(prompt)
-                batch_task_id_list.append(batch_task_ids[t])
-            t = t + 1
-        responses = model.batch(prompts)
-        for task_id, response in zip(batch_task_id_list, responses):
-            completion = filter_code(response.content)
-            samples.append({
-                "task_id": task_id,
-                "completion": completion
-            })
-            cnt = cnt + 1
-            writer = get_stream_writer()
+
+    with tqdm(total=total_samples, desc="生成进度") as pbar:
+        for case_id in range(0, total_samples, batch_size):
+            prompts = []
+            batch_task_id_list = []
+            for case_i in range(case_id, min(case_id + batch_size, total_samples), 1):
+                prompts.append(problems[all_task_ids[case_i // num_samples_per_task]]['prompt'])
+                batch_task_id_list.append(all_task_ids[case_i // num_samples_per_task])
+            responses = model.batch(prompts)
+            for task_id, response in zip(batch_task_id_list, responses):
+                completion = response.content
+                samples.append({
+                    "task_id": task_id,
+                    "completion": completion
+                })
+                cnt = cnt + 1
+                writer = get_stream_writer()
+                pbar.update(1)
             if writer:
                 writer(StreamEvent(
                     current=state['current'],
                     progress=round(cnt/total_samples, 1),
-                    message="常规任务样本合成进度",
+                    message="code任务样本合成进度",
                     data={"progress_detail": f"{cnt}/{total_samples}"}
                 ).json())
-
     write_jsonl(test_case_path, samples)
 
     logger.info(f"\n===== 生成完成 =====")
@@ -130,12 +115,12 @@ def generate_sample_sql(state):
     )
     test_case_path = judger_state['eval_test_case_path']
     problem_path = judger_state['eval_problem_path']
-
+    num_samples_per_task = judger_state['eval_case_num']
     batch_size = judger_state['eval_batch_size']
     problems = read_problems(problem_path)
     all_task_ids = list(problems.keys())
     total_tasks = len(all_task_ids)
-    num_samples_per_task = batch_size
+    
     total_samples = total_tasks * num_samples_per_task
 
     writer = get_stream_writer()
@@ -143,7 +128,7 @@ def generate_sample_sql(state):
         writer(StreamEvent(
             current=state['current'],
             progress=0.0,
-            message="常规任务样本合成开始",
+            message="code任务样本合成开始",
             data={"msg": f"任务总数：{total_tasks}|每个任务样本数：{num_samples_per_task}|总样本数：{total_samples}"}
         ).json())
     logger.info(f"\n===== 开始生成样本 =====")
@@ -156,19 +141,13 @@ def generate_sample_sql(state):
 
     cnt = 0
     with tqdm(total=total_samples, desc="生成进度") as pbar:
-        for batch_idx in range(0, total_tasks, batch_size):
-            batch_task_ids = all_task_ids[batch_idx:batch_idx + batch_size]
-            
+        for case_id in range(0, total_samples, batch_size):
             prompts = []
             batch_task_id_list = []
-            t = 0
-            for task_id in batch_task_ids:
-                prompt = problems[task_id]['prompt']
-                for i in range(0,num_samples_per_task,1):
-                    prompts.append(prompt)
-                    batch_task_id_list.append(batch_task_ids[t])
-                t = t + 1
-            responses = model.batch(prompts,config={"max_tokens": 32768})
+            for case_i in range(case_id, min(case_id + batch_size, total_samples), 1):
+                prompts.append(problems[all_task_ids[case_i // num_samples_per_task]]['prompt'])
+                batch_task_id_list.append(all_task_ids[case_i // num_samples_per_task])
+            responses = model.batch(prompts)
             for task_id, response in zip(batch_task_id_list, responses):
                 completion = response.content
                 samples.append({
@@ -180,14 +159,14 @@ def generate_sample_sql(state):
                 })
                 cnt = cnt + 1
                 writer = get_stream_writer()
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=round(cnt/total_samples, 1),
-                        message="SQL任务样本合成进度",
-                        data={"progress_detail": f"{cnt}/{total_samples}"}
-                    ).json())
                 pbar.update(1)
+            if writer:
+                writer(StreamEvent(
+                    current=state['current'],
+                    progress=round(cnt/total_samples, 1),
+                    message="text2sql任务样本合成进度",
+                    data={"progress_detail": f"{cnt}/{total_samples}"}
+                ).json())
 
     write_jsonl(test_case_path, samples)
 
