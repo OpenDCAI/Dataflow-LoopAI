3b23d4b63b3cee9c155ebfe50baf3a11510335e3
baobaobao <82161965+brjbrj@users.noreply.github.com>
2026-01-17 16:23:32 +0800

    Add output_dir to required fields and update paths

--- loopai/agents/Judger/judger_agent.py
+++ loopai/agents/Judger/judger_agent.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Any, Dict, List, Optional, Type
 
 from langgraph.graph import StateGraph
@@ -40,7 +41,7 @@ class JudgerAgent(BaseAgent):
         def check_required_fields(state: LoopAIState, runtime: Runtime[RuntimeContext]):
             required_fields = {
                 'judger':["eval_model_path", "eval_base_url", "eval_api_key", "eval_temperature",
-                               "eval_top_p", "eval_test_case_path", "eval_problem_path", "eval_result_path", "eval_batch_size", "eval_case_num", "eval_task_type"]
+                               "eval_top_p", "eval_problem_path", "eval_batch_size", "eval_case_num", "eval_task_type", "output_dir"]
             }
             missing_fields = get_missing_fields(required_fields, state)
 
@@ -50,9 +51,11 @@ class JudgerAgent(BaseAgent):
                 is_true = bool(value)
                 if not is_true:
                     missing_fields.setdefault("judger", []).append(key)
-                    
+            '''检查text2sql必要字段'''
+            if not missing_fields:
+                if state.get("judger", {}).get("eval_text2sql_dir", "") == "text2sql":
+                    missing_fields = get_missing_fields({'judger':["eval_text2sql_dir"]}, state)
             if missing_fields:
-                print(missing_fields)
                 state['exception'] = 'ConfigerError'
                 state['next_to'] = 'config_node'
                 state['automated_query'] = self.prompt_loader(
@@ -64,24 +67,25 @@ class JudgerAgent(BaseAgent):
                     update=state,
                     goto=goto_node,
                     graph=Command.PARENT
-                )
+                )           
         return check_required_fields
+
     @staticmethod
     @BaseAgent.set_current
     def data_format_node(state: LoopAIState) -> LoopAIState:
-        problem_format_path = state.get("judger", {}).get("eval_problem_format_path", "")
         problem_path = state.get("judger", {}).get("eval_problem_path", "")
-        format_type = state.get("judger", {}).get("eval_format_type", "human-eval")
+        format_type = state.get("judger", {}).get("eval_format_type", None)
+        state_task_id = state.get("task_id")
+        output_dir = state.get("judger", {}).get("output_dir", "/")
+        problem_file_name = os.path.splitext(os.path.basename(problem_path))[0]
         writer = get_stream_writer()
-        if(format_type == "" or format_type is None):
-            format_type = "None"
-        if(problem_format_path != "" and problem_format_path is not None):
+        if(format_type != "" and format_type is not None):
             if writer:
                 writer(StreamEvent(
                     current=state['current'],
                     progress=0.0,
                     message="任务数据格式化开始",
-                    data={"msg": f"由[{problem_path}]以[{format_type}]格式转存为[{problem_format_path}]"}
+                    data={"msg": f"由[{problem_path}]以[{format_type}]格式转存为[{output_dir}{state_task_id}/{problem_file_name}_format.jsonl]"}
                 ).json())
             data_format(state)
             if writer:
@@ -89,16 +93,16 @@ class JudgerAgent(BaseAgent):
                     current=state['current'],
                     progress=1.0,
                     message="任务数据格式化完成",
-                    data={"msg": f"后续将以[{problem_format_path}]作为问题数据"}
+                    data={"msg": f"后续将以[{output_dir}{state_task_id}/{problem_file_name}_format.jsonl]作为问题数据"}
                 ).json())
-            state["judger"]["eval_problem_path"] = state["judger"]["eval_problem_format_path"]
+            state["judger"]["eval_problem_path"] = f"{output_dir}{state_task_id}/{problem_file_name}_format.jsonl"
         else:
             if writer:
                 writer(StreamEvent(
                     current=state['current'],
                     progress=1.0,
                     message="任务数据格式化结束",
-                    data={"msg": '未设置[eval_problem_format_path]参数，跳过数据格式化过程'}
+                    data={"msg": '未设置[eval_format_type]参数，跳过数据格式化过程'}
                 ).json())
         return state
 
@@ -107,86 +111,64 @@ class JudgerAgent(BaseAgent):
     def generate_node(state: LoopAIState) -> LoopAIState:
         writer = get_stream_writer()
         problem_path = state.get("judger", {}).get("eval_problem_path", "")
-        test_case_path = state.get("judger", {}).get("eval_test_case_path", "")
-        batch_size = state.get("judger", {}).get("eval_batch_size", 10)
+        case_num = state.get("judger", {}).get("eval_case_num", 10)
         task_type = state.get("judger", {}).get("eval_task_type", "code")
+        problem_file_name = os.path.splitext(os.path.basename(problem_path))[0]
+        state_task_id = state.get("task_id")
+        output_dir = state.get("judger", {}).get("output_dir", "/")
+
+        if writer:
+            writer(StreamEvent(
+                current=state['current'],
+                progress=0.0,
+                message=f"{task_type}任务样本合成开始",
+                data={"msg": f"对[{problem_path}]每个问题生成[{case_num}]条样例数据"}
+            ).json())
+
         match task_type:
             case "code":  # code
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=0.0,
-                        message="code任务样本合成开始",
-                        data={"msg": f"对[{problem_path}]每个问题生成[{batch_size}]条样例数据"}
-                    ).json())
                 generate_sample(state)
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=1.0,
-                        message="code任务样本合成完成",
-                        data={"msg": f"结果保存为[{test_case_path}]"}
-                    ).json())
-                return state
             case "text2sql":  # text2sql
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=0.0,
-                        message="text2sql任务样本合成开始",
-                        data={"msg": f"对[{problem_path}]每个问题生成[{batch_size}]条样例数据"}
-                    ).json())
                 generate_sample_sql(state)
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=1.0,
-                        message="text2sql任务样本合成完成",
-                        data={"msg": f"结果保存为[{test_case_path}]"}
-                    ).json())
-                return state
+
+        if writer:
+            writer(StreamEvent(
+                current=state['current'],
+                progress=1.0,
+                message=f"{task_type}任务样本合成完成",
+                data={"msg": f"结果保存为[{output_dir}{state_task_id}/{problem_file_name}_sample.jsonl]"}
+            ).json())
+
+        return state
 
     @staticmethod
     @BaseAgent.set_current
     def evaluate_node(state: LoopAIState) -> LoopAIState:
         task_type = state.get("judger", {}).get("eval_task_type", "code")
+        
+        res = {}
+
         writer = get_stream_writer()
+        if writer:
+            writer(StreamEvent(
+                current=state['current'],
+                progress=0.0,
+                message=f"{task_type}任务评测样本开始",
+                data={"msg": ''}
+            ).json())
         match task_type:
             case "code":  # code
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=0.0,
-                        message="code任务评测样本开始",
-                        data={"msg": ''}
-                    ).json())
                 res = evaluate_sample(state)
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=1.0,
-                        message="code任务评测样本结果",
-                        data=res
-                    ).json())
-                return state
             case "text2sql":
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=0.0,
-                        message="text2sql任务评测样本开始",
-                        data={"msg": ''}
-                    ).json())
                 res = evaluate_sample_sql(state)
-                if writer:
-                    writer(StreamEvent(
-                        current=state['current'],
-                        progress=1.0,
-                        message="text2sql任务评测样本结束",
-                        data=res
-                    ).json())
-                return state
-
+        if writer:
+            writer(StreamEvent(
+                current=state['current'],
+                progress=1.0,
+                message=f"{task_type}任务评测样本结果",
+                data=res
+            ).json())
+        return state
     def init_graph(self, **kwargs):
         builder = StateGraph(LoopAIState)
         builder.add_node("check_required_fields", self.get_check_required_fields_node())
