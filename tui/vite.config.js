25dc97ed291ce9df8713de9ded673d8f8cdd7baa
alever_sn <759636538@qq.com>
2026-07-31 15:27:54 +0800

    [fix] Fixed vue-tui package source issue.

--- tui/vite.config.js
+++ tui/vite.config.js
@@ -2,12 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-const vueTuiSrc = fileURLToPath(new URL('../../vue-tui/src/index.ts', import.meta.url))
-const vueTuiVueSrc = fileURLToPath(new URL('../../vue-tui/src/vue.ts', import.meta.url))
-const vueTuiCliSrc = fileURLToPath(new URL('../../vue-tui/src/cli.ts', import.meta.url))
-const vueTuiMarkdownSrc = fileURLToPath(new URL('../../vue-tui/src/markdown.ts', import.meta.url))
-const vueTuiExperimentalSrc = fileURLToPath(new URL('../../vue-tui/src/experimental.ts', import.meta.url))
-
 const terminalExternal = [
   'fs',
   'node:fs',
@@ -32,16 +26,7 @@ const terminalExternal = [
 
 export default defineConfig(({ mode }) => {
   const base = {
-    plugins: [vue()],
-    resolve: {
-      alias: [
-        { find: /^@simon_he\/vue-tui$/, replacement: vueTuiSrc },
-        { find: /^@simon_he\/vue-tui\/vue$/, replacement: vueTuiVueSrc },
-        { find: /^@simon_he\/vue-tui\/cli$/, replacement: vueTuiCliSrc },
-        { find: /^@simon_he\/vue-tui\/markdown$/, replacement: vueTuiMarkdownSrc },
-        { find: /^@simon_he\/vue-tui\/experimental$/, replacement: vueTuiExperimentalSrc }
-      ]
-    }
+    plugins: [vue()]
   }
 
   if (mode === 'terminal') {
